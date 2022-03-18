ad17bc7236980421c974da2a504a601df4c31366
Pelayo García Álvarez <pelayo.garcia-alvarez@capgemini.com>
2022-03-18 11:11:47 +0100

    Menu arreglado

--- src/app/shared/menu.module.ts
+++ src/app/shared/menu.module.ts
@@ -1,16 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MenuComponent } from './menu/menu.component';
+import { AppRoutingModule } from '../app-routing.module';
 
 
 
 @NgModule({
   declarations: [MenuComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    AppRoutingModule
   ],
   exports:[
-    MenuComponent,
+    MenuComponent
   ]
 })
 export class MenuModule { }
