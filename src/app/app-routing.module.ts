f96098b0d591d9f7d19f86410b5bf3f331e6106a
psariego <pablo.sariego-alonso@capgemini.com>
2022-03-18 09:16:23 +0100

    Añadidas rutas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ArticulosComponent } from './tienda/articulos/articulos.component';
+import { ClientesComponent } from './tienda/clientes/clientes.component';
+import { ComprasComponent } from './tienda/compras/compras.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', component: ComprasComponent, pathMatch:'full'},
+  {path:'articulos', component: ArticulosComponent},
+  {path:'clientes', component: ClientesComponent},
+  {path:'**', redirectTo: ''}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
