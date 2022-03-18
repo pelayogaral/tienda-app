4fa8c7c61e694b27e75ea1225d6431fdd9124b6b
psariego <pablo.sariego-alonso@capgemini.com>
2022-03-18 10:07:55 +0100

    Añadido interface

---
+++ src/app/tienda/interfaces/compra.ts
@@ -0,0 +1,11 @@
+export interface Cliente{
+  codCliente: number;
+  nombre: string;
+  empresa: string;
+  puesto: string;
+  cp: number;
+  provincia: string;
+  telefono: number;
+  fechaNacimiento: Date;
+
+}
